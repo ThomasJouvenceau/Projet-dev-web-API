3d3d3331c927315789153efde5dd88597be439bd
Louja971 <mcjael971@gmail.com>
2024-12-20 18:28:32 +0100

    kb

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -4,25 +4,36 @@ const User = require("../models/user");
 module.exports = (roles = []) => {
   return async (req, res, next) => {
     const headerValue = req.headers.authorization ?? req.headers.Authorization;
-    if (!headerValue) return res.sendStatus(401);
+    if (!headerValue) {
+      console.log("Authorization header missing");
+      return res.sendStatus(401);
+    }
 
     const [type, token] = headerValue.split(/\s+/);
-    if (type !== "Bearer") return res.sendStatus(401);
+    if (type !== "Bearer") {
+      console.log("Invalid token type");
+      return res.sendStatus(401);
+    }
 
     try {
       const payload = jwt.verify(token, process.env.JWT_SECRET);
       const user = await User.findByPk(payload.id);
 
-      if (!user || !user.activated) return res.sendStatus(401);
+      if (!user || !user.activated) {
+        console.log("User not found or not activated");
+        return res.sendStatus(401);
+      }
 
       // Vérification du rôle si nécessaire
       if (roles.length > 0 && !roles.includes(user.role)) {
+        console.log("User does not have the required role");
         return res.sendStatus(403); // Forbidden
       }
 
       req.user = user;
       next();
     } catch (error) {
+      console.error("Error during authentication", error);
       return res.sendStatus(401);
     }
   };
