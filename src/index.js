11b789802af239f2c106cbdfc99011111ff993c6
ThomasJouvenceau <thomas.jouvenceau@edu.devinci.fr>
2024-12-20 18:56:39 +0100

    index

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ app.use(express.json());
 app.get("/", (request, response, next) => {
   response.send("Bienvenue dans notre API");
 });
+
+app.use(userRouter); 
+app.use(teamRouter); 
+app.use(adminRouter); 
+app.use(arbitreRouter);
+
+app.listen(process.env.PORT, () =>
+    console.log("Server listening on port " + process.env.PORT)
+  );
