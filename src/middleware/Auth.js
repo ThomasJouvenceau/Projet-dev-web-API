5448fdc80ce1f29b73be4a7e38dd4349cf17f907
Louja971 <mcjael971@gmail.com>
2024-12-22 16:36:38 +0100

    Auth

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -1,40 +1,42 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+const Equipe = require("../models/equipe");
+const Arbitre = require("../models/arbitre");
+const Admin = require("../models/admin");
 
-module.exports = (roles = []) => {
-  return async (req, res, next) => {
-    const headerValue = req.headers.authorization ?? req.headers.Authorization;
-    if (!headerValue) {
-      console.log("Authorization header missing");
-      return res.sendStatus(401);
+module.exports = async (req, res, next) => {
+  const headerValue = req.headers.authorization ?? req.headers.Authorization;
+
+  if (!headerValue) return res.sendStatus(401);
+  const [type, token] = headerValue.split(/\s+/);
+
+  if (type !== "Bearer") return res.sendStatus(401);
+  try {
+    const payload = jwt.verify(
+      token,
+      process.env.JWT_SECRET ?? "Tres$ecureKey321$$V3RY$$trong"
+    );
+
+    if (payload.role === "user") {
+      req.user = await User.findByPk(payload.id);
+      if (!req.user) return res.sendStatus(401);
+      if (!req.user.activated) return res.sendStatus(403);
+    } else if (payload.role === "admin") {
+      req.user = await Admin.findByPk(payload.id);
+      if (!req.user) return res.sendStatus(401);
+    } else if (payload.role === "arbitre") {
+      req.user = await Arbitre.findByPk(payload.id);
+      if (!req.user || !req.user.approvedByAdmin) return res.sendStatus(403);
+    } else if (payload.role === "equipe") {
+      req.user = await Equipe.findByPk(payload.id);
+      if (!req.user || !req.user.approvedByAdmin) return res.sendStatus(403);
+    } else {
+      return res.sendStatus(403);
     }
 
-    const [type, token] = headerValue.split(/\s+/);
-    if (type !== "Bearer") {
-      console.log("Invalid token type");
-      return res.sendStatus(401);
-    }
-
-    try {
-      const payload = jwt.verify(token, process.env.JWT_SECRET);
-      const user = await User.findByPk(payload.id);
-
-      if (!user || !user.activated) {
-        console.log("User not found or not activated");
-        return res.sendStatus(401);
-      }
-
-      // Vérification du rôle si nécessaire
-      if (roles.length > 0 && !roles.includes(user.role)) {
-        console.log("User does not have the required role");
-        return res.sendStatus(403); // Forbidden
-      }
-
-      req.user = user;
-      next();
-    } catch (error) {
-      console.error("Error during authentication", error);
-      return res.sendStatus(401);
-    }
-  };
+    req.user.role = payload.role;
+    next();
+  } catch (error) {
+    return res.sendStatus(401);
+  }
 };
