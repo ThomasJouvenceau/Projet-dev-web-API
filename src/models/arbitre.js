dbec779a7f1bc404348ce3c277282fee0d35b022
ThomasJouvenceau <thomas.jouvenceau@edu.devinci.fr>
2024-12-22 13:15:22 +0100

    création des models arbitre et match

---
+++ src/models/arbitre.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const connection = require("./db");
+const bcrypt = require("bcryptjs");
+
+class Arbitre extends Model {}
+
+Arbitre.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: connection,
+    tableName: "arbitres",
+  }
+);
+
+Arbitre.addHook("beforeCreate", async (arbitre) => {
+  const salt = await bcrypt.genSalt(10);
+  arbitre.password = await bcrypt.hash(arbitre.password, salt);
+});
+
+Arbitre.addHook("beforeUpdate", async (arbitre, { fields }) => {
+  if (fields.includes("password")) {
+    const salt = await bcrypt.genSalt(10);
+    arbitre.password = await bcrypt.hash(arbitre.password, salt);
+  }
+});
+
+module.exports = Arbitre;
---
+++ src/models/match.js
@@ -0,0 +1,46 @@
+class Match extends Model {}
+
+Match.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    equipe1_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'equipes',
+        key: 'id',
+      },
+      allowNull: false,
+    },
+    equipe2_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'equipes',
+        key: 'id',
+      },
+      allowNull: false,
+    },
+    arbitre_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'arbitres',
+        key: 'id',
+      },
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: connection,
+    tableName: "matches",
+  }
+);
+
+module.exports = Match;
